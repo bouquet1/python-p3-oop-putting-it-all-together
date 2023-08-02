85e1fe1bb85e26c901a6f698f8d5283ba10dc63b
bouquet1 <bukuleta@gmail.com>
2023-08-01 19:08:31 -0700

    all tests passed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    def __init__(self, brand, size):
+    def __init__(self, brand, size, condition = None):
         self.brand = brand
         self.size = size
+        self._condition = condition
 
     def get_size(self):
         return self._size
@@ -15,3 +16,21 @@ class Shoe:
             self._size = size
         
     size = property(get_size, set_size)
+
+    @property
+    def condition(self):
+        return self._condition
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        # Set the condition to "New"
+        self._condition = "New"
+
+
+# try python lib/shoe.py to check
+stan_smith = Shoe("Adidas", 9)
+stan_smith.cobble()
+print(stan_smith.condition)
+
+  
+
